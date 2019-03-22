78f861726110108775d10a780606bb5937207bf3
Lisa Leslie <lisar@ibouge.com>
2019-03-22 16:28:10 -0700

    initial routes set up files created

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsData = [{
+    "name":"Tom",
+    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ],
+      sum: 36
+  
+}]
