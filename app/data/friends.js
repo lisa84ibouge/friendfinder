dd40a6cc17f7cbd9169817075491bc30e2d4566b
Lisa Leslie <lisar@ibouge.com>
2019-03-26 14:45:59 -0700

    updated array of friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,139 +1,8 @@
 var friends = [
-    {
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-
-
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-
-
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-}, 
-{
-    "name":"Tom",
-    "photo":"https://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ],
-      sum: 36
-  
-} , 
+    
 {
     "name":"John Leslie",
-    "photo":"https://scontent-sea1-1.xx.fbcdn.net/v/t31.0-8/14706887_10154280928168813_3462136691240314764_o.jpg?_nc_cat=107&_nc_ht=scontent-sea1-1.xx&oh=190fd732334999ebe80c7fe5d9fe05e8&oe=5D0F389Chttps://www.usmagazine.com/wp-content/uploads/tom-from-myspace-lg.jpg",
+    "photo":"https://scontent-sea1-1.xx.fbcdn.net/v/t31.0-8/14706887_10154280928168813_3462136691240314764_o.jpg?_nc_cat=107&_nc_ht=scontent-sea1-1.xx&oh=190fd732334999ebe80c7fe5d9fe05e8&oe=5D0F389C",
     "scores":[
         4,
         3,
@@ -148,11 +17,55 @@ var friends = [
       ],
       sum: 33
   
-} 
+}, {"name": "Sue Rowlett",
+    "photo": "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22728821_10212820592383909_1810203666508657687_n.jpg?_nc_cat=103&_nc_ht=scontent-sea1-1.xx&oh=7138724ed1238ba9a4ad4e191677f324&oe=5D13B9DD",
+    "scores":[
+      3,
+      4,
+      3,
+      4,
+      2,
+      1,
+      3,
+      5,
+      5,
+      5
+    ],
+    sum: 35
+}, {"name": "Bonnie Leslie", 
+   "photo": "https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/56334633_267595830839086_4441637268062470144_n.jpg?_nc_cat=102&_nc_ht=scontent-sea1-1.xx&oh=8e63d593e37cdf32311f47b03432cf10&oe=5D1901DE",
+   "scores": [
+      4,
+      3,
+      2,
+      5,
+      4,
+      1,
+      3,
+      4,
+      1,
+      4
+
+   ]
+  }, {"name": "Dan Leslie", 
+    "photo": "https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/54798335_867349330263611_7910703346841812992_n.jpg?_nc_cat=109&_nc_ht=scontent-sea1-1.xx&oh=6118e646d0a79ee56b721211e456e07d&oe=5D49ED5D",
+    "scores": [
+
+      5,
+      3,
+      3,
+      1,
+      2,
+      3,
+      5,
+      4, 
+      1,
+      5
+],
 
 
 
-
-];
+  }
+]
 
 module.exports = friends;
